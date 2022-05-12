7a58471601348161140e7cf3d2ff001129c9b1d0
82241012 <kdk124578@naver.com>
2022-05-12 20:35:31 +0900

    Init project

---
+++ src/main/java/com/study/keepdak/KeepdakApplication.java
@@ -0,0 +1,13 @@
+package com.study.keepdak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepdakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepdakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/keepdak/KeepdakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.keepdak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepdakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
